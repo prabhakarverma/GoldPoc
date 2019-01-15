24ef0e6556cf9b198ae7ea3533451f9b8f82b815
pverma3028 <verma.prabhakar8@gmail.com>
2019-01-15 23:32:36 +0530

    initia commot

---
+++ src/main/java/com/gold/GoldPoc/GoldPocApplication.java
@@ -0,0 +1,15 @@
+package com.gold.GoldPoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldPocApplication {
+
+	public static void main(String[] args) {
+		System.out.println("app started...");
+		SpringApplication.run(GoldPocApplication.class, args);
+	}
+
+}
+
