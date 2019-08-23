48fa92a90ce5c51be7fa42ba5e1e2455048a2846
pverma3028 <verma.prabhakar8@gmail.com>
2019-08-23 21:12:28 +0530

    commit model and update gradle file

--- src/main/java/com/gold/GoldPoc/model/User.java
+++ src/main/java/com/gold/GoldPoc/model/User.java
@@ -1,5 +1,7 @@
 package com.gold.GoldPoc.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Date;
@@ -8,84 +10,32 @@ import java.sql.Date;
  * @author Prabhakar Verma
  */
 
+@Data
 @Entity
-@Table(name = "user")
-public class User implements Serializable {
+@Table( name = "user" )
+public class User implements Serializable
+{
 
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue( strategy = GenerationType.AUTO )
+    @Column( name = "id", unique = true, nullable = false )
     private Long id;
 
-    @Column(name = "name", length = 100)
+    @Column( name = "name", length = 100 )
     private String name;
 
-    @Column(name = "password", length = 50)
+    @Column( name = "password", length = 50 )
     private String password;
 
-    @Column(name = "email", length = 100)
+    @Column( name = "email", length = 100 )
     private String email;
 
-    @Column(name = "dob")
+    @Column( name = "dob" )
     private String dob;
 
-    @Column(name = "create_time", columnDefinition = "DATE DEFAULT NULL")
-    private Date createTime ;
+    @Column( name = "create_time", columnDefinition = "DATE DEFAULT NULL" )
+    private Date createTime;
 
-    public User() {
-    }
-
-    public User(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
 }
